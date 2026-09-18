62ec6163a291b5554cf87fcd6301c84b9f5216ec
pharmakarios1 <akhigbeblessed@gmail.com>
2026-09-18 15:54:12 +0100

    adding Nat gatway to the private subnet

--- backend.tf
+++ backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "umbrella-terraform-state-blessed-2026"
-    key = "umbrella/dev/terraform.tfstate"
-    region = "us-east-1"
+    bucket       = "umbrella-terraform-state-blessed-2026"
+    key          = "umbrella/dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
   }
 }
--- main.tf
+++ main.tf
@@ -85,6 +85,11 @@ resource "aws_route_table_association" "public_b" {
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
   tags = {
     Name = "${local.name_prefix}-private-rt"
   }
@@ -99,3 +104,6 @@ resource "aws_route_table_association" "private_b" {
   subnet_id      = aws_subnet.private_b.id
   route_table_id = aws_route_table.private.id
 }
+
+
+
